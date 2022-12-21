05c2fe53d4e5b798e8e0ad1742e5411c3fd6f787
nasimkjiniya <nasimkjiniya@gmail.com>
2022-12-20 17:28:39 -0800

    Added NavigationUI to the MainActivity and also created a menu option and added it to the navigation from TitleFragment and override it's methods.

--- app/src/main/java/com/example/android/navigation/TitleFragment.kt
+++ app/src/main/java/com/example/android/navigation/TitleFragment.kt
@@ -1,12 +1,12 @@
 package com.example.android.navigation
 
 import android.os.Bundle
+import android.view.*
 import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import androidx.navigation.Navigation
+import androidx.navigation.findNavController
+import androidx.navigation.ui.NavigationUI
 import com.example.android.navigation.databinding.FragmentTitleBinding
 
 /**
@@ -27,6 +27,8 @@ class TitleFragment : Fragment() {
         // Inflate the layout for this fragment
         val binding : FragmentTitleBinding = DataBindingUtil.inflate(inflater,R.layout.fragment_title,container,false)
 
+        setHasOptionsMenu(true)
+
 //        simple way to navigate
 //        binding.playButton.setOnClickListener()
 //        {
@@ -37,5 +39,15 @@ class TitleFragment : Fragment() {
         binding.playButton.setOnClickListener(Navigation.createNavigateOnClickListener(R.id.action_titleFragment2_to_gameFragment))
 
         return binding.root
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        super.onCreateOptionsMenu(menu, inflater)
+        inflater?.inflate(R.menu.overflow_menu,menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return NavigationUI.onNavDestinationSelected(item, requireView().findNavController())
+                || super.onOptionsItemSelected(item)
     }
 }
